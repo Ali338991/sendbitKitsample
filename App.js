f532974a6c2a66ba1ca60713a0befd4ff4255c1c
Apple <apple@Apples-MacBook-Pro.local>
2023-02-06 21:25:29 +0500

    initial

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ChatScreens from './screen';
+
+const App = () => {
+  return <ChatScreens />;
+};
+
+
+
+export default App;
---
+++ screen/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignInScreen from './SignIn';
+import {
+  GroupChannelListScreen,
+  GroupChannelCreateScreen,
+  GroupChannelScreen,
+} from './chat';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  createNativeClipboardService,
+  createNativeFileService,
+  createNativeMediaService,
+  createNativeNotificationService,
+  SendbirdUIKitContainer,
+  useSendbirdChat,
+} from '@sendbird/uikit-react-native';
+
+import Clipboard from '@react-native-clipboard/clipboard';
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
+import RNFBMessaging from '@react-native-firebase/messaging';
+import Video from 'react-native-video';
+import * as DocumentPicker from 'react-native-document-picker';
+import * as FileAccess from 'react-native-file-access';
+import * as ImagePicker from 'react-native-image-picker';
+import * as Permissions from 'react-native-permissions';
+import * as CreateThumbnail from 'react-native-create-thumbnail';
+
+const ClipboardService = createNativeClipboardService(Clipboard);
+const NotificationService = createNativeNotificationService({
+  messagingModule: RNFBMessaging,
+  permissionModule: Permissions,
+});
+const FileService = createNativeFileService({
+  fsModule: FileAccess,
+  permissionModule: Permissions,
+  imagePickerModule: ImagePicker,
+  mediaLibraryModule: CameraRoll,
+  documentPickerModule: DocumentPicker,
+});
+const MediaService = createNativeMediaService({
+  VideoComponent: Video,
+  thumbnailModule: CreateThumbnail,
+});
+const RootStack = createNativeStackNavigator();
+const Navigation = () => {
+  const {currentUser} = useSendbirdChat();
+
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{headerShown: false}}>
+        {!currentUser ? (
+          <RootStack.Screen name={'SignIn'} component={SignInScreen} />
+        ) : (
+          <>
+            <RootStack.Screen
+              name={'GroupChannelList'}
+              component={GroupChannelListScreen}
+            />
+            <RootStack.Screen
+              name={'GroupChannelCreate'}
+              component={GroupChannelCreateScreen}
+            />
+            <RootStack.Screen
+              name={'GroupChannel'}
+              component={GroupChannelScreen}
+            />
+          </>
+        )}
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const ChatScreens = () => {
+  return (
+    <SendbirdUIKitContainer
+    appId={'9E5EBD2B-A396-411D-BFEB-48EE2D4E5FCD'}
+      chatOptions={{localCacheStorage: AsyncStorage}}
+      platformServices={{
+        file: FileService,
+        notification: NotificationService,
+        clipboard: ClipboardService,
+      }}>
+      <Navigation />
+    </SendbirdUIKitContainer>
+  );
+};
+export default ChatScreens;
